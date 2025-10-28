58dd62424e54a8ace31172d5a31283895ef69482
Michael DAndrea <mdandrea2006@gmail.com>
2025-10-28 17:12:12 -0500

    Brief about us

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,32 @@
 function Home() { 
     return (
-        // home page content here
         <div className="home">
             <h1>Home Page</h1>
+            
+            <div style={{
+                backgroundColor: '#535bf2', 
+                border: '5px solid #00010e', 
+                height: '350px', 
+                width: '300px', 
+                padding: '20px', 
+                margin:  'auto'
+            }}> 
+                <h2 style={{textAlign: 'center'}}>Our Purpose:</h2>
+                <hr />
+                <pre style={{
+                    whiteSpace: 'pre-wrap',
+                    wordWrap: 'break-word',
+                    fontFamily: 'inherit',
+                    margin: 0
+                }}>
+Our purpose is to strengthen the bonds of brotherhood among the men of Atonement. 
+Through this fellowship, we are empowered to serve the Lord by actively supporting
+our church and community. A key part of our mission is to raise essential funds
+for those important projects and needs that fall outside the regular budget, 
+ensuring our school, church, and community have the resources to thrive.
+                </pre>
+            </div>
         </div>
     );
 }
-
 export default Home;
