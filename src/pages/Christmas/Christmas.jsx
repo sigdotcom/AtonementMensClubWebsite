c957f430505e52a11dde574ca75cea30a3c3efe9
Michael DAndrea <mdandrea2006@gmail.com>
2025-10-29 16:13:50 -0500

    fixed Christmas problem

--- src/pages/Christmas/Christmas.jsx
+++ src/pages/Christmas/Christmas.jsx
@@ -1,33 +1,43 @@
+import treeLot from "./TreeLot.png";
+
 function Christmas() {
     return (
         <div className="christmas">
-        <Helmet>
-            <title> Christmas Events</title>
-            <meta name= "Christmas Page" content = "All things Christmas"/>
-        </Helmet>
-        <h1 className="center-text">
-         <u> Christmas Tree lot </u>
-        </h1>
-        <hr></hr>
-        <pre className=" Christmaspre">
-            Welcome to our tree lot! We proudly offer a
-            fantastic
-            selection of quality Frasier Fir, Balsam Fir, and
-            Scotch Pine 
-            trees, along with pine roping, tree stands, and tree kits
+            <h1 style={{ textAlign: 'center' }}>
+                <u>Christmas Tree Lot</u>
+            </h1>
 
-            A heartfelt thank you to all our returning 
-            customers-we
-            cant wait to see you again this year! if you
-            haven't visited
-            us before, we invite you to stop by and check out
-            our
-            beautiful trees and friendly service.
-        </pre>
-        <img src = "TreeLot.png" alt = "Christmas Tree Lot"
-        style ={{bottom: "0px", right: "0px", width: "150px",}}
-        />
+            <hr />
+
+            <pre style={{
+                whiteSpace: 'pre-wrap',
+                wordWrap: 'break-word',
+                fontFamily: 'inherit',
+                margin: 0
+            }}>
+Welcome to our tree lot! We proudly offer a fantastic
+selection of quality Frasier Fir, Balsam Fir, and Scotch Pine
+trees, along with pine roping, tree stands, and tree kits
+
+A heartfelt thank you to all our returning customers we
+can't wait to see you again this year! If you haven't visited
+us before, we invite you to stop by and check out our
+beautiful trees and friendly service.
+            </pre>
+
+            <img
+                src={treeLot}
+                alt="Christmas Tree Lot"
+                style={{
+                    position: 'fixed',
+                    bottom: '0px',
+                    right: '0px',
+                    height: '200px',
+                    width: '600px'
+                }}
+            />
         </div>
     );
 }
+
 export default Christmas;
