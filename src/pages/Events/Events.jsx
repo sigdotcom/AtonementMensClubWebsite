8d8919fa5d0b62b0b3022de587795c92ddfe8619
Cole Nemkovich <colenemko@gmail.com>
2025-10-29 18:00:10 -0500

    Minor Events

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,7 +1,11 @@
 function Events() {
     return (
         <div className="events">
-
+            <div className="minorEvents">
+                <h1>Minor Events...</h1>
+                <h3>This will go further down..</h3>
+                <h3>Coal was here hehe</h3>
+            </div>
         </div>
     );
 }   
