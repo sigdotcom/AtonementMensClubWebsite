647a1162003e1db99ba2ee6db2029ebb67f9b7f6
Michael DAndrea <mdandrea2006@gmail.com>
2025-10-30 08:05:46 -0500

    Updated blurb

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,20 +5,20 @@ function Home() {
             
             <div style={{
                 border: '5px solid #00010e', 
-                height: '250px',         
+                height: '280px',         
                 width: '450px',         
                padding: '10px 20px 20px 10px',
                margin: '40px auto',    
             
             }}> 
-                <h2 style={{textAlign: 'center', marginTop: '-5px'  }}>Our Purpose:</h2>
-                <hr/>
+                <h2 style={{textAlign: 'center', marginTop: '-3px'  }}>Our Purpose:</h2>
+                <hr></hr>
                 <pre style={{
                     whiteSpace: 'pre-wrap',
                     wordWrap: 'break-word',
                     fontFamily: 'inherit',
                     fontSize: '18px',
-                    margin: 0
+                    marginBottom: '-2px'
                 }}>
 Our purpose is to strengthen the bonds of brotherhood among the men of Atonement. 
 Through this fellowship, we are empowered to serve the Lord by actively supporting
@@ -26,6 +26,7 @@ our church and community. A key part of our mission is to raise essential funds
 for those important projects and needs that fall outside the regular budget, 
 ensuring our school, church, and community have the resources to thrive.
                 </pre>
+                
             </div>
         </div>
     );
