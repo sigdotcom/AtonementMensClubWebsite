ddd33d52724ff9e9b61754dbd1f86703038f7378
Michael DAndrea <mdandrea2006@gmail.com>
2025-10-31 12:35:08 -0500

    Made about us Blurb look better on page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,21 +4,24 @@ function Home() {
             <h1>Home Page</h1>
             
             <div style={{
-                border: '5px solid #00010e', 
-                height: '280px',         
-                width: '450px',         
-               padding: '10px 20px 20px 10px',
-               margin: '40px auto',    
+                border: '5px solid rgba(0, 0, 0, 0.05)',
+                boxShadow: '2px 1px 2px rgba(0,0,0,0.06), 0 6px 16px rgba(0,0,0,0.10), 0 18px 36px rgba(0,0,0,0.08)', 
+                height: 'auto',         
+                width: 'min(90%, 450px)',         
+               padding: '15px 20px 20px 20px',
+               margin: '50px auto',  
+             
             
             }}> 
-                <h2 style={{textAlign: 'center', marginTop: '-3px'  }}>Our Purpose:</h2>
-                <hr></hr>
+                <h2 style={{textAlign: 'center', marginTop: '0', marginBottom: '10px'  }}>Our Purpose:</h2>
+                <hr style={{ borderTop: '-1px solid rgba(9, 0, 0, 0.15)', margin: '0 0 5px'}} />
                 <pre style={{
                     whiteSpace: 'pre-wrap',
                     wordWrap: 'break-word',
-                    fontFamily: 'inherit',
-                    fontSize: '18px',
-                    marginBottom: '-2px'
+                    fontFamily: 'arial, sans-serif',
+                    fontSize: '19px',
+                    marginBottom: '-2px',
+                    marginTop: '12px'
                 }}>
 Our purpose is to strengthen the bonds of brotherhood among the men of Atonement. 
 Through this fellowship, we are empowered to serve the Lord by actively supporting
