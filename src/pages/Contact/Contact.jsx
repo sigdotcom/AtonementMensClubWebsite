a9908e745042c76f4efba779f7550466b758f054
Britney Choimah <bchoimah@gmail.com>
2025-11-04 02:15:23 -0600

    layout done

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,7 +4,10 @@ function Contact() {
   return (
     //About page here
     <div className="contact">
-      <p>contact page</p>
+      <div className="contactBlurbCon">
+        <h1>CONTACT US</h1>
+        <p>contact content here</p>
+      </div>
     </div>
   );
 }
