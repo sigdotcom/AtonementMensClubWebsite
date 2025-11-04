d4404fb2b9714f3130daaa529eb44f4c44ed30c5
Britney Choimah <bchoimah@gmail.com>
2025-11-04 00:55:55 -0600

    quick changes

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,13 @@
+import "./about.css";
+
 function About() {
-    return (
-        //About page here
-        <div className="about">
-            <p>About page</p>
-            <p>ahahah i made changes</p>
-        </div>
-    );
+  return (
+    //About page here
+    <div className="about">
+      <p>About page</p>
+      <p>ahahah i made changes</p>
+    </div>
+  );
 }
 
 export default About;
