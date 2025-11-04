caf54fe57219b45cee3b272fa89c2131bc42017f
Britney Choimah <bchoimah@gmail.com>
2025-11-04 01:49:50 -0600

    christmas layout done

--- src/pages/Christmas/Christmas.jsx
+++ src/pages/Christmas/Christmas.jsx
@@ -1,10 +1,16 @@
-
+import "./Christmas.css";
 function Christmas() {
-    return (
-        <div className="christmas">
-           
-        </div>
-    );
+  return (
+    <div className="christmas">
+      <div className="mainCon">
+        <p>main christmas content here</p>
+      </div>
+      <div className="christmasMenuCon">
+        <h1>MENU</h1>
+        <p>christmas menu content here</p>
+      </div>
+    </div>
+  );
 }
 
 export default Christmas;
