6b215d362bc08972610e30ee56558ce5a8c76ded
Britney Choimah <bchoimah@gmail.com>
2025-11-04 02:08:44 -0600

    added contact page

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import "./Contact.css";
+
+function Contact() {
+  return (
+    //About page here
+    <div className="contact">
+      <p>contact page</p>
+    </div>
+  );
+}
+
+export default Contact;
