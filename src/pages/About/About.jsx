bbf4ad72f0d6bb2cfdb3464ae32ef07c548d1ba1
Lucas Gehner <lmggxf@umsystem.edu>
2025-11-05 17:24:31 -0600

    Made about us title

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,7 +5,7 @@ function About() {
     //About page here
     <div className="about">
       <div className="aboutMainCon">
-        <p>all the main about content here</p>
+        <h1 style={{color: "#3993DD", fontSize: "64px"}}>ABOUT US</h1>
       </div>
       <div className="imgGalleryCon">
         <p>image gallery here</p>
