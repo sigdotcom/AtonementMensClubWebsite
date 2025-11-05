80f452c12175c2ba0698056a47d4c9656e1293fa
Jayla Chambers <jmc53k@umsystem.edu>
2025-11-05 17:54:56 -0600

    11/5 working on menu

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,8 +1,15 @@
+
 function Menu() {
     return (
         //menu page here
         <div className="menu">
             <p>Menu page</p>
+            <div className="bbqHours" style="text-align: center;">
+                <p>bbq hours here</p>
+            </div>
+            <div className="bbqMenu">
+                <p>bbq menu here</p>
+            </div>
         </div>
     );
 }
