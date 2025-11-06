534fbae90aa5b0738f19ef62b81aa9663b8bb3d4
Mariah Loggins <mclrwd@umsystem.edu>
2025-11-05 18:12:02 -0600

    working on updates

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -8,7 +8,7 @@ function About() {
         <h1 style={{color: "#3993DD", fontSize: "64px"}}>ABOUT US</h1>
       </div>
       <div className="imgGalleryCon">
-        <p>image gallery here</p>
+        <p>image gallery here sending updates lol dw</p>
       </div>
     </div>
   );
