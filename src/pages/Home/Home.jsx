cb0e54e4a5325642102f35d8c4225644f07c50b7
Michael DAndrea <mdandrea2006@gmail.com>
2025-11-06 18:56:07 -0600

    fixed merge problem

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,44 +1,4 @@
-<<<<<<<<< Temporary merge branch 1
 import "./Home.css";
-
-function Home() {
-  return (
-    <div className="home">
-      <h1>Home Page</h1>
-
-      <div
-        style={{
-          border: "5px solid #00010e",
-          height: "250px",
-          width: "450px",
-          padding: "10px 20px 20px 10px",
-          margin: "40px auto",
-        }}
-      >
-        <h2 style={{ textAlign: "center", marginTop: "-5px" }}>Our Purpose:</h2>
-        <hr />
-        <pre
-          style={{
-            whiteSpace: "pre-wrap",
-            wordWrap: "break-word",
-            fontFamily: "inherit",
-            fontSize: "18px",
-            margin: 0,
-          }}
-        >
-          Our purpose is to strengthen the bonds of brotherhood among the men of
-          Atonement. Through this fellowship, we are empowered to serve the Lord
-          by actively supporting our church and community. A key part of our
-          mission is to raise essential funds for those important projects and
-          needs that fall outside the regular budget, ensuring our school,
-          church, and community have the resources to thrive.
-        </pre>
-      </div>
-    </div>
-  );
-}
-export default Home;
-=========
 function Home() { 
     return (
         <div className="home">
@@ -51,8 +11,6 @@ function Home() {
                 width: 'min(90%, 450px)',         
                padding: '15px 20px 20px 20px',
                margin: '50px auto',  
-             
-            
             }}> 
                 <h2 style={{textAlign: 'center', marginTop: '0', marginBottom: '10px'  }}>Our Purpose:</h2>
                 <hr style={{ borderTop: '-1px solid rgba(9, 0, 0, 0.15)', margin: '0 0 5px'}} />
@@ -70,7 +28,6 @@ our church and community. A key part of our mission is to raise essential funds
 for those important projects and needs that fall outside the regular budget, 
 ensuring our school, church, and community have the resources to thrive.
                 </pre>
-                
             </div>
         </div>
     );
