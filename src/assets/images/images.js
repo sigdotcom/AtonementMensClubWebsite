34c91d3686c4fc87da5db7a35eaba38359b0366d
Mariah Loggins <mclrwd@umsystem.edu>
2025-11-12 17:56:53 -0600

    Mariah added 38 pictures to the assets for the about page

---
+++ src/assets/images/images.js
@@ -0,0 +1,195 @@
+import image1 from './img-gallery/Back_of_House.jpg'
+
+const imgGallery = [
+    {
+        P_no: 1,
+        P_img: image1
+    },
+
+    {
+        P_no: 2,
+        P_img: image2
+    },
+
+    {
+        P_no: 3,
+        P_img: image3
+    },
+
+    {
+        P_no: 4,
+        P_img: image4
+    },
+
+    {
+        P_no: 5,
+        P_img: image5
+    },
+
+    {
+        P_no: 6,
+        P_img: image6
+    },
+
+    {
+        P_no: 7,
+        P_img: image7
+    },
+
+    {
+        P_no: 8,
+        P_img: image8
+    },
+
+    {
+        P_no: 9,
+        P_img: image9
+    },
+
+    {
+        P_no: 10,
+        P_img: image10
+    },
+
+    {
+        P_no: 11,
+        P_img: image11
+    },
+
+    {
+        P_no: 12,
+        P_img: image12
+    },
+
+    {
+        P_no: 13,
+        P_img: image13
+    },
+
+    {
+        P_no: 14,
+        P_img: image14
+    },
+
+    {
+        P_no: 15,
+        P_img: image15
+    },
+
+    {
+        P_no: 16,
+        P_img: image16
+    },
+
+    {
+        P_no: 17,
+        P_img: image17
+    },
+
+    {
+        P_no: 18,
+        P_img: image18
+    },
+
+    {
+        P_no: 19,
+        P_img: image19
+    },
+
+    {
+        P_no: 20,
+        P_img: image20
+    },
+
+    {
+        P_no: 21,
+        P_img: image21
+    },
+
+    {
+        P_no: 22,
+        P_img: image22
+    },
+
+    {
+        P_no: 23,
+        P_img: image23
+    },
+
+    {
+        P_no: 24,
+        P_img: image24
+    },
+
+    {
+        P_no: 25,
+        P_img: image25
+    },
+
+    {
+        P_no: 26,
+        P_img: image26
+    },
+
+    {
+        P_no: 27,
+        P_img: image27
+    },
+
+    {
+        P_no: 28,
+        P_img: image28
+    },
+
+    {
+        P_no: 29,
+        P_img: image29
+    },
+
+    {
+        P_no: 30,
+        P_img: image30
+    },
+
+    {
+        P_no: 31,
+        P_img: image31
+    },
+
+    {
+        P_no: 32,
+        P_img: image32
+    },
+
+    {
+        P_no: 33,
+        P_img: image33
+    },
+
+    {
+        P_no: 34,
+        P_img: image34
+    },
+
+    {
+        P_no: 35,
+        P_img: image35
+    },
+
+    {
+        P_no: 36,
+        P_img: image36
+    },
+
+    {
+        P_no: 37,
+        P_img: image37
+    },
+
+    {
+        P_no: 38,
+        P_img: image38
+    },
+
+]
+export default imgGallery;
