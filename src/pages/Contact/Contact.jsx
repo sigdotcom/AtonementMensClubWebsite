2c7b52771da5a964e8c24ab72aee7d6d5e7d1718
Britney Choimah <bchoimah@gmail.com>
2025-11-19 22:00:00 -0600

    fran contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -7,11 +7,43 @@ function Contact() {
     <div className="contact">
       <div className="contactBlurbCon">
         <h1>CONTACT US</h1>
-        <p>contact content</p>
-        <div className="left-side">
-          <img src={image} alt="Contact Photo" />
+
+        <div className="contactblock">
+          <div className="left-side">
+            <img src="https://www.nomadfoods.com/wp-content/uploads/2018/08/placeholder-1-e1533569576673-960x960.png"></img>
+          </div>
+
+          <div className="blurb">
+            <h2>Contact | Contact | Contact</h2>
+            <p>Lorem ipsum doset.</p>
+          </div>
         </div>
-        
+
+        <div className="contactblock">
+          <div className="left-side">
+            <img src="https://www.nomadfoods.com/wp-content/uploads/2018/08/placeholder-1-e1533569576673-960x960.png"></img>
+          </div>
+
+          <div className="blurb">
+            <h2>Contact | Contact | Contact</h2>
+            <p>Lorem ipsum doset.</p>
+          </div>
+        </div>
+
+        <div className="contactblock">
+          <div className="left-side">
+            <img src="https://www.nomadfoods.com/wp-content/uploads/2018/08/placeholder-1-e1533569576673-960x960.png"></img>
+          </div>
+
+          <div className="blurb">
+            <h2>Contact | Contact | Contact</h2>
+            <p>Lorem ipsum doset.</p>
+          </div>
+        </div>
+
+        <a href="LINK TO MAILING LIST HERE :D">
+          <button>Join the Mailing List</button>
+        </a>
       </div>
     </div>
   );
